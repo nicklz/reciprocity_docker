ed2b6d75a72fc2c9c08d545978c770c2e0191559
Nick Kuhn <nicklz22@yahoo.com>
2017-11-09 12:37:42 -0800

    fix

--- data/www/scripts/rebuild.sh
+++ data/www/scripts/rebuild.sh
@@ -6,8 +6,8 @@
 
 
 ## MySQL
-echo "drop database labiomed;" | mysql -uroot -h mysql
-echo "create database labiomed;" | mysql -uroot -h mysql
+echo "drop database reciprocity;" | mysql -uroot -h mysql
+echo "create database reciprocity;" | mysql -uroot -h mysql
 
 
 
@@ -18,7 +18,7 @@ echo "create database emp_dir;" | mysql -uroot -h mysql
 echo "SYNC DATABASE"
 echo "=================="
 
-mysql -u root -h mysql -p labiomed --password="" < /var/www/dump.sql
+mysql -u root -h mysql -p reciprocity --password="" < /var/www/dump.sql
 
 
 
@@ -28,9 +28,9 @@ echo "=================="
 mysql -u root -h mysql -p emp_dir --password="" < /var/www/dump2.sql
 
 
-cd /var/www/project/labiomed && composer install --no-interaction --no-dev --prefer-dist
+cd /var/www/project/reciprocity && composer install --no-interaction --no-dev --prefer-dist
 
-cd /var/www/project/labiomed/web/sites/default && drush updb -y
-cd /var/www/project/labiomed/web/sites/default && drush cr -y
-cd /var/www/project/labiomed/web/sites/default && drush cim -y
-cd /var/www/project/labiomed/web/sites/default && drush cr -y
+cd /var/www/project/reciprocity/web/sites/default && drush updb -y
+cd /var/www/project/reciprocity/web/sites/default && drush cr -y
+cd /var/www/project/reciprocity/web/sites/default && drush cim -y
+cd /var/www/project/reciprocity/web/sites/default && drush cr -y
