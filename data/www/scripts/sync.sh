8626c7511eebac865193b4385f2c64d475cf3aec
Nick Kuhn <nicklz22@yahoo.com>
2017-11-15 17:05:51 -0800

    fix

--- data/www/scripts/sync.sh
+++ data/www/scripts/sync.sh
@@ -11,8 +11,8 @@ echo "create database reciprocity;" | mysql -uroot -h mysql
 echo "SYNC DATABASE"
 echo "=================="
 
-drush @pantheon.uceap-reciprocity.qa sql-dump --result-file=/var/www/dump.sql
-mysql -u root -h mysql -p reciprocity --password="" < /var/www/dump.sql
+drush @pantheon.uceap-reciprocity.qa sql-dump | mysql -u root -h mysql -p reciprocity
+mysqldump -u root -h mysql -p reciprocity > /var/www/dump.sql
 
 
 
