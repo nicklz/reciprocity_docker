f24a5a5cf22606427b71ca2b0d1594d95b62bcbf
Nick Kuhn <nicklz22@yahoo.com>
2017-12-13 12:11:30 -0800

    fix

--- data/www/scripts/deploy.sh
+++ data/www/scripts/deploy.sh
@@ -11,3 +11,8 @@ terminus remote:drush uceap-reciprocity.rws-$1 --  recd
 echo "-----------------------------------------------"
 echo "http://rws-$1-uceap-reciprocity.pantheonsite.io"
 echo "-----------------------------------------------"
+
+
+echo "--------------------------------------------"
+echo "https://uceapit.atlassian.net/browse/RWS-$1"
+echo "--------------------------------------------"
